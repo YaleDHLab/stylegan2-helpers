fc27e1c6040eb4ee82ae1c98ed30cf64f892622e
Jake Kara <jake@jakekara.com>
2020-08-28 10:11:18 -0400

    make DATA_DIR parent of datasets folder

--- config.sh
+++ config.sh
@@ -3,8 +3,11 @@
 echo "Loading configuration..."
 
 # Update below with path to your files
-# The source images cropped square (eg. 1024x1024)
-IMAGE_DIR="/home/dhlab/EveryPixel/2020 Data/resized/1024-1024-crops/"
 
-# These you don't need to touch
-DATA_DIR="$IMAGE_DIR/datasets"
+# The source images cropped square (eg. 1024x1024)
+IMAGE_DIR=/home/dhlab/EveryPixel/2020\ Data/resized/1024-1024-crops
+
+# These you don't need to touch. These scripts are meant to be run 
+# from the repository root directory. The datasets will be in a 
+# subdirectory of $DATA_DIR named "datasets"
+DATA_DIR="$(pwd)"
