05fe5cd97adaef36e00dbe63cab5bfd51323b72b
Jake Kara <jake.kara@yale.edu>
2020-09-04 13:02:57 -0400

    make create-datasets.sh stay alive via nohup

--- create-datasets.sh
+++ create-datasets.sh
@@ -9,4 +9,4 @@ set -e
 echo "* Running StyleGAN2:dataset.py"
 echo "*     Image dir: $IMAGE_DIR"
 echo "*      Data dir: $DATA_DIR"
-python stylegan2/dataset_tool.py create_from_images "$DATA_DIR" "$IMAGE_DIR"
+nohup python stylegan2/dataset_tool.py create_from_images "$DATA_DIR/datasets" "$IMAGE_DIR" & 
